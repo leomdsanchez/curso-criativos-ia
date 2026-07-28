3c4e9c15dbaed535a6afa32e34f02ecbca7683e2
leomdsanchez <leomdsanchez@gmail.com>
2026-07-28 16:45:04 -0300

    refactor: present pillars without repeated descriptions

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CoursePortal } from './CoursePortal'
 import './styles.css'
+import './pillar-summary.css'
 
 const root = document.getElementById('root')
 
